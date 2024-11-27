d8d98149b245f086971fa8e95581a7b1167f8c16
John Wilger <john@johnwilger.com>
2024-11-27 11:02:22 -0800

    Update internal-v2.nix

--- internal-v2.nix
+++ internal-v2.nix
@@ -1,6 +1,6 @@
-{ nodejs-22, jq, openssl, coreutils, stdenv, mkShell, lib, fetchurl, writeText, writeShellScript, runCommand, fetchFromGitHub }:
+{ nodejs_22, jq, openssl, coreutils, stdenv, mkShell, lib, fetchurl, writeText, writeShellScript, runCommand, fetchFromGitHub }:
 rec {
-  default_nodejs = nodejs-22;
+  default_nodejs = nodejs_22;
 
   ## helper functions that allow users to define common source override mechanism
 
